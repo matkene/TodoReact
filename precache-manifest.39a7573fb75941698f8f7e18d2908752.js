2342e2313ec5375953ae0b61d7d3b4087db879d9
Chizea Matthew <matkenefaiths@gmail.com>
2020-08-27 17:55:42 +0100

    Updates

---
+++ precache-manifest.39a7573fb75941698f8f7e18d2908752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911fbcdc56f61640c08277f75b1d5dc2",
+    "url": "/TodoReact/index.html"
+  },
+  {
+    "revision": "adddd6db3e8fbb50e069",
+    "url": "/TodoReact/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "f3e95598d5ce445ba539",
+    "url": "/TodoReact/static/js/2.61b499ce.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/TodoReact/static/js/2.61b499ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adddd6db3e8fbb50e069",
+    "url": "/TodoReact/static/js/main.93bd14ee.chunk.js"
+  },
+  {
+    "revision": "15cfa5fa51ddb81287a7",
+    "url": "/TodoReact/static/js/runtime-main.ff963568.js"
+  }
+]);
